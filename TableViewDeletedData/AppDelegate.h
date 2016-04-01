08a0b583706ace1ba2ffae3a33a0e55f12605057
Borsche <borsche@borschedemacbook-pro.local>
2016-04-01 11:15:51 +0800

    Initial Commit

---
+++ TableViewDeletedData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewDeletedData
+//
+//  Created by Borsche on 16/4/1.
+//  Copyright © 2016年 Borsche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewDeletedData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewDeletedData
+//
+//  Created by Borsche on 16/4/1.
+//  Copyright © 2016年 Borsche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
